35d88dcacbec329bacb0326556119f9530dcacb1
Vignesh Sankaran <developer@sankaran.io>
2016-09-14 20:15:57 +1000

    Finally got this thing to print out a UUID

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,21 +8,21 @@ fn main()
 {
     let mut server = Nickel::new();
 /*
+    server.get("/newUUID", middleware! { |_, mut response|
+        response.set(MediaType::Json);
+
+        "{'uuid': 'surprise'}"
+    });
+*/
     server.get("/newUUID", middleware! { |request, mut response|
         let uuid = Uuid::new_v4();
-        let mut json_string = "{\"uuid\": ".to_owned();
+        let mut json_string = "{'uuid': '".to_owned();
         json_string.push_str(&uuid.to_string());
-        json_string.push_str(" }");
+        json_string.push_str("' }");
 
         response.set(MediaType::Json);
-        response.send(json_string);
+        json_string
+    });
 
-    });
-    */
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Hello world!"
-        }
-    });
     server.listen("127.0.0.1:10000");
 }
