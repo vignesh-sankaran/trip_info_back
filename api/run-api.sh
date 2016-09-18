61f052655a73f6b8634c7c7ec7aec1cb9234355a
Vignesh Sankaran <vignesh@eduroam-173.wireless.rmit.edu.au>
2016-09-18 13:21:04 +1000

    Fixed tech debt

--- api/run-api.sh
+++ api/run-api.sh
@@ -1,3 +1,3 @@
-docker rm -f rustapi && \
+docker rm -f rustapi
 docker build -t rust:api ./ && \
-docker run -d -p 20000:20000 -P --name rustapi rust:api
+docker run -d -p 20000:20000 -h localhost --name rustapi rust:api
--- api/src/main.rs
+++ api/src/main.rs
@@ -8,11 +8,11 @@ fn main()
 {
     let mut server = Nickel::new();
 
-    server.get("/newUUID", middleware! { |request, mut response|
+    server.get("/newUUID", middleware! { |_, mut response|
         let uuid = Uuid::new_v4();
-        let mut json_string = "{'uuid': '".to_owned();
+        let mut json_string = "{\"uuid\": \"".to_owned();
         json_string.push_str(&uuid.to_string());
-        json_string.push_str("' }");
+        json_string.push_str("\" }");
 
         response.set(MediaType::Json);
         json_string
