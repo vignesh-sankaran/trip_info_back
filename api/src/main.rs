e260f269887c56886caab2ad1db798df4f1ce5cf
Vignesh Sankaran <vignesh@Vigneshs-MacBook-Pro.local>
2016-09-21 19:37:29 +1000

    Working on getting this DB to be accessible

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate nickel;
 extern crate uuid;
+#[macro_use(bson, doc)]
 extern crate bson;
 extern crate mongodb;
 
 use nickel::{Nickel, HttpRouter, MediaType};
 use uuid::Uuid;
 use bson::Bson;
-use mongodb::{Client, ThreadedClient};
+use mongodb::{Client, ThreadedClient, CommandType};
+use mongodb::error::Error::OperationError;
 use mongodb::db::ThreadedDatabase;
 
 fn main()
@@ -16,12 +18,18 @@ fn main()
 
     server.get("/newUUID", middleware! { |_, mut response|
         let uuid = Uuid::new_v4();
+        let uuid_string = &uuid.to_string();
         let mut json_string = "{\"uuid\": \"".to_owned();
-        json_string.push_str(&uuid.to_string());
+        json_string.push_str(uuid_string);
         json_string.push_str("\" }");
 
-        let client = Client::conect("localhost", 27017).ok().except("Could not connect to users DB");
-        let usersDb = client.db("admin").co
+        let client = Client::connect("localhost", 27017).ok().expect("Could not connect to users DB");
+        let auth_db = client.db("auth");
+        auth_db.auth("system", "system").unwrap();        
+
+        let users_coll = client.db("tripinfo").collection("users");
+        let id_entry = doc!{"id" => uuid_string};
+        users_coll.insert_one(id_entry, None).ok().expect("Failed to insert new user ID into users DB");
 
         response.set(MediaType::Json);
         json_string
